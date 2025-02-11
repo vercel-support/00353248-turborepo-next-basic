31d9f8d1a39e92b87ed5fe89c38a6fb299103cd1
Nick Vigier <nvigier46@gmail.com>
2025-02-11 13:55:27 +0000

    test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
+            testGet started by editing <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
